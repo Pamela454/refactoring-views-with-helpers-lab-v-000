c5cf74a9e047a78d0b7ec71f869fe42aaaf99163
Pamela Frieze <pfrieze@gmail.com>
2018-11-05 01:46:13 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,7 +1,7 @@
 module ArtistsHelper
   def display_artist(song)
-    binding.pry
     if song.artist
+      binding.pry
       link_to song.artist.name, song.artist
     else
       link_to 'Add Artist', edit_song_path(song)
